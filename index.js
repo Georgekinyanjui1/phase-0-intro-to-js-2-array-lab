3118492beb6dd61947e17de4782a17035a578741
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-04 13:22:22 +0300

    commit tree b44acda17ae3104ec31d7d6ede98c0643dd66c8d

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, "Broom"]
+    return appendCat
+}
+
+function prependCat(name) {
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+
+function removeLastCat() {
+    const prependCat = cats.slice()
+    prependCat.pop()
+    return prependCat
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice()
+    removeFirstCat.shift()
+    return removeFirstCat
+
+}
